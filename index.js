e87d97fd6f3a32836305f1e88e29fb2479cee842
Uchechukwu-Udefi <udefihenry@gmail.com>
2024-11-16 11:42:32 +0200

    Update cards and sum element in both html and js

--- index.js
+++ index.js
@@ -10,10 +10,14 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
 // CASH OUT!
 
 function startGame() {
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
