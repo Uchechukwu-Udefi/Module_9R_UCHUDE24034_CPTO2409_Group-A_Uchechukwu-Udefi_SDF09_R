9ff109d23c8958371c95121fbf4499a12eea48ed
Uchechukwu-Udefi <udefihenry@gmail.com>
2024-11-16 09:29:44 +0200

    Create css file and update h1 element in html

--- index.js
+++ index.js
@@ -7,18 +7,21 @@ let firstCard = 10
 let secondCard = 11
 let sum = firstCard + secondCard
 let hasBlackJack = false
+let isAlive = true
+let message = ""
 
 if (sum < 20) {
-    console.log("Do you want to draw a new card? 🙂")
+    message = "Do you want to draw a new card? 🙂"
 } else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳")
+    message = "Wohoo! You've got Blackjack! 🥳"
     hasBlackJack = true
 } else {
-    console.log("You're out of the game! 😭")
+    message = "You're out of the game! 😭"
+    isAlive = false
 }
 
 // CASH OUT!
-console.log(hasBlackJack)
+console.log(message)
 
 
 
