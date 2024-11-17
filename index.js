7b616345d8c8be71c5a2970bda3a57ddd847e73a
Uchechukwu-Udefi <udefihenry@gmail.com>
2024-11-18 00:51:19 +0200

    Update new card funtion in js

--- index.js
+++ index.js
@@ -55,11 +55,13 @@ function renderGame() {
 }
 
 function newCard() {
+    if (isAlive === true && hasBlackJack === false) {
     let card = getRandomCard()
     sum += card
     cards.push(card)
     console.log(cards)
     renderGame()
+    }
 }
 
 
