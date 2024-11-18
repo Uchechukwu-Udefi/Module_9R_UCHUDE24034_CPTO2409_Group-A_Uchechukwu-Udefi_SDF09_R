3197ab1545c7ae064aa33734ac998c59c2a68be3
Uchechukwu-Udefi <udefihenry@gmail.com>
2024-11-18 03:58:06 +0200

    Update box element animation

--- index.js
+++ index.js
@@ -70,3 +70,20 @@ function newCard() {
     }
 }
 
+
+
+
+
+
+
+let box = document.getElementById("box")
+
+box.animate([
+  { transform: 'scaleX(1)', offset: 0 },
+  { transform: 'scaleX(2)', offset: 0.3 },
+  { transform: 'scaleX(0.5)', offset: 0.6 },
+  { transform: 'scaleX(1)', offset: 1 }
+], {
+  duration: 1000,
+  iterations: 999
+});
